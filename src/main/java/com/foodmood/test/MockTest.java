f65f26c05dccdaa049a0148fb3663d6e522f2a4c
leoboehm <runnix@gmail.com>
2016-04-28 11:07:24 +0200

    pom change and junit

---
+++ src/main/java/com/foodmood/test/MockTest.java
@@ -0,0 +1,16 @@
+package com.foodmood.test;
+import org.mockito.Mockito.*;
+
+import org.junit.*;
+import org.junit.Test;
+
+public class MockTest {
+	
+
+@Test
+public void testCreateRecipe()  {
+  //  create mock
+  
+}
+
+}
